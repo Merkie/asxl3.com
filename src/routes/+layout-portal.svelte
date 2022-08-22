10218df7667fdfd2a498acd5643d14765cae5fa0
Merkie <merkie@post.com>
2022-08-22 12:01:37 -0500

    Add prisma

--- src/routes/+layout-portal.svelte
+++ src/routes/+layout-portal.svelte
@@ -4,8 +4,6 @@
 	$: cssVarStyles = Object.entries(Colors)
 		.map(([key, value]) => `--${key}:${value}`)
 		.join(';');
-
-	console.log(Colors);
 </script>
 
 <div style={cssVarStyles}><slot /></div>
