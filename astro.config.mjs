7e99e3e4ade5cc52947e4c536951656a49b13366
Merkie <merkie@post.com>
2023-02-11 16:14:24 -0600

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  // output: 'server',
-  // adapter: vercel()
+  output: 'server',
+  adapter: netlify()
 });
